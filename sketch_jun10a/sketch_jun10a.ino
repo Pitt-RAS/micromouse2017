42d6dcddc86a22fbbe88ec609e58059247f41c72
dmgilmour <dmgilmour@pitt.edu>
2017-07-15 19:16:08 -0400

    Buttons!

--- sketch_jun10a/sketch_jun10a.ino
+++ sketch_jun10a/sketch_jun10a.ino
@@ -27,31 +27,21 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  bool b1 = false;
-  bool b2 = false;
-  bool b3 = false;
-  while(!(b1 || b2 || b3)) {
-    if (digitalRead(button1)) {
-      Serial.println("b1\n");
-      b1 = true;
-    }
-    if (digitalRead(button2)) {
-      Serial.println("b2\n");
-      b2 = true;
-    }
-    if (digitalRead(button3)) {
-      Serial.println("b3\n");
-      b3 = true;
-    }
+
+
+  // Pressing any button will take it out of standby and run the program
+  delay(100);
+  while(!(digitalRead(button1) || digitalRead(button2) || digitalRead(button3))) {
+     move(0);
+     Serial.println("Standby");
   }
-  move(32);
-  delay(1000);
-  move(0);
-  delay(1000);
-  Serial.println("b1 " + b1);
-  Serial.println("b2 " + b2);
-  Serial.println("b3 " + b3);
+  delay(100);
+
+  while(!(digitalRead(button1) || digitalRead(button2) || digitalRead(button3))) {
+    move(32);
+  }
+
+  
 }
 
 void move(int velocity) {
