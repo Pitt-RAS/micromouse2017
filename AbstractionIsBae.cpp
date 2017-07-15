097aab1644f75ce70aa2af0889431677674e98fa
Dale Erikson <dje23@pitt.edu>
2017-07-15 19:14:06 -0400

    Add files via upload

---
+++ AbstractionIsBae.cpp
@@ -0,0 +1,74 @@
+#include <Arduino.h>
+
+int Ain1 = 4;
+int Ain2 = 2;
+int PWMA = 3;
+int Bin1 = 6;
+int Bin2 = 7;
+int PWMB = 5;
+int button1 = 21;
+int button2 = 22;
+int button3 = 23;
+int sensorData = 0;
+int sensorPin = A14;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(Ain1, OUTPUT);
+  pinMode(Ain2, OUTPUT);
+  pinMode(PWMA, OUTPUT);
+  pinMode(Bin1, OUTPUT);
+  pinMode(Bin2, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  bool b1 = false;
+  bool b2 = false;
+  bool b3 = false;
+  while(!(b1 || b2 || b3)) {
+    if (digitalRead(button1)) {
+      Serial.println("b1\n");
+      b1 = true;
+    }
+    if (digitalRead(button2)) {
+      Serial.println("b2\n");
+      b2 = true;
+    }
+    if (digitalRead(button3)) {
+      Serial.println("b3\n");
+      b3 = true;
+    }
+  }
+  move(32);
+  delay(1000);
+  move(0);
+  delay(1000);
+  Serial.println("b1 " + b1);
+  Serial.println("b2 " + b2);
+  Serial.println("b3 " + b3);
+  sensorData = getSensorData(sensorPin);
+  Serial.println("Distance " + sensorData);
+  
+}
+
+void move(int velocity) {
+  digitalWrite(Ain1, HIGH);
+  digitalWrite(Ain2, LOW);
+  analogWrite(PWMA, velocity);
+  digitalWrite(Bin1, LOW);
+  digitalWrite(Bin2, HIGH);
+  analogWrite(PWMB, velocity);
+}
+
+int getSensorData(int pin)
+{
+  return analogRead(pin);
+}
+
